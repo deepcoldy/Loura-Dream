21b92b150a7a672b7c19306c376b5569e4488764
DeepCold <deepcold@DeepColds-iMac.local>
2018-07-23 01:11:13 +0800

    add background image

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,53 +1,93 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div id="hahatest"></div>
+    <!-- <img src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="layer">
+      <div id="canvas"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import { Scene, Sprite } from "spritejs";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      ratio: 0
+    };
+  },
+  created() {
+    this.ratio = window.screen.availWidth / window.screen.availHeight;
   },
   mounted() {
-    const scene = new Scene("#hahatest", {
-      viewport: ["800", "800"],
-      resolution: [3080, 800]
+    let stickMode = "height";
+    if (this.ratio > 750 / 1331) stickMode = "width";
+    const scene = new Scene("#canvas", {
+      viewport: ["auto", "auto"],
+      resolution: [750, 1331],
+      stickMode
     });
 
     const layer = scene.layer();
-
-    const robot = new Sprite(
-      "https://p5.ssl.qhimg.com/t01c33383c0e168c3c4.png"
+    const background = new Sprite(
+      "https://ws4.sinaimg.cn/large/006tKfTcly1ftj1fr5i0fj30ku10zwsd.jpg"
     );
 
-    robot.attr({
-      anchor: [0, 0.5],
-      pos: [0, 0]
+    const girl = new Sprite(
+      "http://zoneke-img.b0.upaiyun.com/2145bf19a6d3ea57ad995b0fe15732f9.png"
+    );
+
+    girl.attr({
+      anchor: 0,
+      pos: [80, 140]
     });
 
-    robot.animate(
-      [
-        { pos: [0, 0] },
-        { pos: [0, 300] },
-        { pos: [2700, 300] },
-        { pos: [2700, 0] }
-      ],
-      {
-        duration: 5000,
-        iterations: Infinity,
-        direction: "alternate"
-      }
-    );
+    // const robot = new Sprite(
+    //   "https://p5.ssl.qhimg.com/t01c33383c0e168c3c4.png"
+    // );
 
-    layer.append(robot);
+    // robot.attr({
+    //   anchor: [0, 0.5],
+    //   pos: [0, 0]
+    // });
+
+    // robot.animate(
+    //   [
+    //     { pos: [0, 0] },
+    //     { pos: [0, 300] },
+    //     { pos: [2700, 300] },
+    //     { pos: [2700, 0] }
+    //   ],
+    //   {
+    //     duration: 5000,
+    //     iterations: Infinity,
+    //     direction: "alternate"
+    //   }
+    // );
+
+    layer.append(background);
+    layer.append(girl);
   }
 };
 </script>
+<style lang="scss" scoped>
+@import "./Home.scss";
+</style>
+<style>
+body,
+html,
+#app,
+.home {
+  width: 100%;
+  height: 100%;
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
