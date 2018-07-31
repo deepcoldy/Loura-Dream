b58f4437961b92238c03ef77423b62e09192f9fb
申晗 <deepcoldy@gmail.com>
2018-07-31 22:51:01 +0800

    commit

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -21,9 +21,11 @@ export default {
       step: 1,
       ratio: 0,
       layer: null,
+      newlayer: null,
       page1: {
         group: null,
         background: null,
+        microphone: null,
         Girl: [],
         GirlIndex: 1,
         Man: [],
@@ -34,7 +36,36 @@ export default {
         GasRight: [],
         GasIndex: 1
       },
-      page2: null,
+      page2: {
+        group: null,
+        background: null,
+        Man1: [],
+        Man1Index: 1,
+        Girl2: [],
+        Girl2Index: 1,
+        Girl3: [],
+        Girl3Index: 1,
+        Eye4: [],
+        Eye4Index: 1,
+        Girl5: [],
+        Girl5Index: 1,
+        Man6: [],
+        Man6Index: 1
+      },
+      transto3: {
+        group: null,
+        transBackground: null
+      },
+      page3: {
+        group: null,
+        background: null,
+        Girl: [],
+        GirlIndex: 1
+      },
+      transto4: {
+        group: null,
+        Sky: null
+      },
       page4: null,
       page1Animate: null,
       sprite: [],
@@ -49,15 +80,16 @@ export default {
   },
 
   mounted() {
-    let stickMode = "height";
-    if (this.ratio > 750 / 1331) stickMode = "width";
-    const scene = new Scene("#canvas", {
-      viewport: ["auto", "auto"],
-      resolution: [750, 1331],
-      stickMode
-    });
-    this.layer = scene.layer();
+    this.Init("#canvas", "layer");
     this.loadAssetsPage1();
+    this.nextPage();
+    setTimeout(() => {
+      this.nextPage();
+    }, 1500);
+    setTimeout(() => {
+      this.nextPage();
+    }, 6000);
+
     // this.loadAssetsPage2();
     // this.layer.append(this.page2);
     // this.loadAssetsPage4();
@@ -77,6 +109,16 @@ export default {
     // });
   },
   methods: {
+    Init(canvasid, name) {
+      let stickMode = "height";
+      if (this.ratio > 750 / 1331) stickMode = "width";
+      const scene = new Scene(canvasid, {
+        viewport: ["auto", "auto"],
+        resolution: [750, 1331],
+        stickMode
+      });
+      this[name] = scene.layer();
+    },
     nextPage() {
       // this.page1Animate = setInterval(() => {
       //   this.page1.removeChild(this.index);
@@ -90,9 +132,10 @@ export default {
       // }, 50);
       if (this.step === 1) {
         clearInterval(this.interval);
+        this.loadAssetsPage1(false);
         this.loadAssetsPage2();
         const flash = new Sprite({
-          size: [750, 1331],
+          size: [750 * 1.5, 1331 * 1.5],
           pos: [0, 0],
           bgcolor: "#fff"
         }).attr({
@@ -104,7 +147,8 @@ export default {
             duration: 500,
             fill: "forwards"
           });
-        }, 100);
+          this.moveToPage2();
+        }, 150);
         // this.page1.group.animate(
         //   [
         //     { size: [750, 1331], pos: [0, 0], borderRadius: 0 },
@@ -116,13 +160,12 @@ export default {
         //     fill: "forwards"
         //   }
         // );
-        // this.page1.background[this.index - 1].animate(
-        //   [{ scale: 1, pos: [0, 0] }, { scale: 0.2, pos: [-10, -20] }],
-        //   {
-        //     duration: 2000,
-        //     fill: "forwards"
-        //   }
-        // );
+      }
+      if (this.step === 2) {
+        this.Page2TransToPage3();
+      }
+      if (this.step === 3) {
+        this.Page3TransToPage4();
       }
       if (this.step === 4) {
         this.page4.animate([{ x: 0 - 10 }, { x: 0 - 750 }], {
@@ -130,13 +173,27 @@ export default {
           duration: 2000
         });
       }
-      // this.step++;
+      this.step++;
     },
-    loadAssetsPage1() {
+    nextFrame(indexName, length, begin, page = "page1", special = "") {
+      if (this[page][indexName] === length - 1) {
+        if (special === "once") return;
+        this[page][indexName] = begin;
+      } else {
+        this[page][indexName] = this[page][indexName] + 1;
+      }
+    },
+    loadAssetsPage1(auto = true) {
       this.page1.group = new Group();
       this.page1.background = new Sprite(
-        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page1/%E8%80%83%E8%AF%95-%E8%83%8C%E6%99%AF.jpg"
+        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page1/%E8%83%8C%E6%99%AF.jpg"
       );
+      this.page1.microphone = new Sprite(
+        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page1/%E9%BA%A6%E5%85%8B%E9%A3%8E.png"
+      ).attr({
+        pos: [122, 400],
+        zIndex: 2
+      });
       for (let index = 0; index <= 21; index++) {
         const Girl = new Sprite(`
         https://test-1255639802.cos.ap-beijing.myqcloud.com/page1/girl/%E9%A2%84%E5%90%88%E6%88%90-3_000${
@@ -162,16 +219,16 @@ export default {
         }.png
         `);
 
-        this.page1.GasLeft.push(
+        this.page1.GasRight.push(
           GasLeft.attr({
             zIndex: 1,
-            pos: [470, 1060]
+            pos: [470, 1090]
           })
         );
-        this.page1.GasRight.push(
+        this.page1.GasLeft.push(
           GasRight.attr({
             zIndex: 1,
-            pos: [42, 1060]
+            pos: [39, 1080]
           })
         );
       }
@@ -206,6 +263,7 @@ export default {
       }
 
       this.page1.group.append(this.page1.background);
+      this.page1.group.append(this.page1.microphone);
       this.page1.group.append(this.page1.Girl[0]);
       this.page1.group.append(this.page1.Man[0]);
       this.page1.group.append(this.page1.Woman[0]);
@@ -230,46 +288,377 @@ export default {
       // );
 
       // }
-      this.autoAnimatPage1();
-    },
-    nextFrame(indexName, length, begin) {
-      // console.log(indexName, length);
-      if (this.page1[indexName] === length - 1) {
-        this.page1[indexName] = begin;
-      } else {
-        this.page1[indexName] = this.page1[indexName] + 1;
-      }
+      auto ? this.autoAnimatPage1() : "";
     },
     autoAnimatPage1() {
       this.interval = setInterval(() => {
         this.page1.group.clear();
         this.page1.group.append(this.page1.background);
+        this.page1.group.append(this.page1.microphone);
         this.page1.group.append(this.page1.Girl[this.page1.GirlIndex]);
         this.page1.group.append(this.page1.GasLeft[this.page1.GasIndex]);
         this.page1.group.append(this.page1.GasRight[this.page1.GasIndex]);
         this.page1.group.append(this.page1.Man[this.page1.ManIndex]);
         this.page1.group.append(this.page1.Woman[this.page1.WomanIndex]);
-        // this.nextFrame("GirlIndex", this.page1.Girl.length);
-        // this.nextFrame("Man", this.page1.Man.length);
         this.nextFrame("GirlIndex", this.page1.Girl.length, 0);
         this.nextFrame("GasIndex", this.page1.GasLeft.length, 0);
         this.nextFrame("ManIndex", this.page1.Man.length, 4);
         this.nextFrame("WomanIndex", this.page1.Woman.length, 4);
-        console.log(this.page1.WomanIndex);
-        // console.log(this.index);
       }, 100);
     },
-    loadAssetsPage2() {
-      const background = new Sprite(
-        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/%E8%81%9A%E4%BC%9A-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87.png"
-      );
-      this.page2 = new Group();
-      background.attr({
-        pos: [-220, -2400],
-        scale: 5.6
+    moveToPage2() {},
+    async loadAssetsPage2() {
+      const scene = new Scene("#canvas", {
+        viewport: ["auto", "auto"],
+        resolution: [750 * 1.5, 1331 * 1.5]
       });
-      this.page2.append(background);
-      this.layer.append(this.page2);
+      this.layer = scene.layer();
+      this.layer.removeChild(this.page1.group);
+      this.page2.background = new Sprite(
+        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/%E8%81%9A%E4%BC%9A%E8%83%8C%E6%99%AF%202.png"
+      );
+      this.page2.group = new Group();
+
+      this.page2.group.append(
+        this.page2.background.attr({
+          zIndex: 4,
+          width: 750 * 1.5,
+          height: 1331 * 1.5
+        })
+      );
+
+      // this.layer.clear();
+
+      this.page2.group.attr({
+        x: -600 * 1.5,
+        y: 1800 * 1.5,
+        anchor: 0.5,
+        scale: 5.5 * 1.5,
+        width: 750 * 1.5,
+        height: 1331 * 1.5
+      });
+      this.page2.group.append(
+        this.page1.group.attr({
+          pos: [485 * 1.5, 330 * 1.5],
+          scale: 0.18 * 1.5,
+          zIndex: 3,
+          width: 750 * 1.5,
+          height: 1331 * 1.5
+        })
+      );
+      // Page2 Man1
+      for (let index = 0; index <= 9; index++) {
+        const Man1 = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/left1/%E8%81%9A%E4%BC%9A-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }-min.png
+        `);
+        Man1.attr({ zIndex: 5 });
+        this.page2.Man1.push(
+          Man1.attr({
+            width: 140 * 1.5,
+            height: 490 * 1.5,
+            pos: [51 * 1.5, 654 * 1.5]
+          })
+        );
+      }
+      // Page2 Girl2
+      for (let index = 0; index <= 9; index++) {
+        const Girl2 = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/meijia/%E7%BE%8E%E5%98%89_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }-min.png
+        `);
+        Girl2.attr({ zIndex: 5 });
+        this.page2.Girl2.push(
+          Girl2.attr({
+            width: 150 * 1.5,
+            height: 350 * 1.5,
+            pos: [148 * 1.5, 777 * 1.5],
+            zIndex: 5
+          })
+        );
+      }
+      // Page2 Girl3
+      for (let index = 0; index <= 9; index++) {
+        const Girl3 = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/left2/%E8%81%9A%E4%BC%9A-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }-min.png
+        `);
+        Girl3.attr({ zIndex: 5 });
+        this.page2.Girl3.push(
+          Girl3.attr({
+            width: 160 * 1.5,
+            height: 380 * 1.5,
+            pos: [277 * 1.5, 755 * 1.5],
+            zIndex: 5
+          })
+        );
+      }
+      // Page2 Eye4
+      for (let index = 0; index <= 9; index++) {
+        const Eye4 = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/eye/%E8%81%9A%E4%BC%9A-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }.png
+        `);
+        Eye4.attr({ zIndex: 5 });
+        this.page2.Eye4.push(
+          Eye4.attr({
+            width: 30 * 1.5,
+            height: 10 * 1.5,
+            pos: [418 * 1.5, 654 * 1.5],
+            zIndex: 5
+          })
+        );
+      }
+      // Page2 Girl5
+      for (let index = 0; index <= 15; index++) {
+        const Girl5 = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/left3/%E8%81%9A%E4%BC%9A-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }-min.png
+        `);
+        Girl5.attr({ zIndex: 5 });
+        this.page2.Girl5.push(
+          Girl5.attr({
+            width: 160 * 1.5,
+            height: 350 * 1.5,
+            pos: [413 * 1.5, 786 * 1.5],
+            zIndex: 5
+          })
+        );
+      }
+      // Page2 Man6
+      for (let index = 0; index <= 10; index++) {
+        const Man6 = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page2/left4/%E8%81%9A%E4%BC%9A-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }-min.png
+        `);
+        Man6.attr({ zIndex: 5 });
+        this.page2.Man6.push(
+          Man6.attr({
+            width: 250 * 1.5,
+            height: 390 * 1.5,
+            pos: [546 * 1.5, 748 * 1.5],
+            zIndex: 5
+          })
+        );
+      }
+      this.page2.group.append(this.page2.Man1[0]);
+      this.page2.group.append(this.page2.Girl2[0]);
+      this.page2.group.append(this.page2.Girl3[0]);
+      this.page2.group.append(this.page2.Eye4[0]);
+      this.page2.group.append(this.page2.Girl5[0]);
+      this.page2.group.append(this.page2.Man6[0]);
+
+      this.layer.append(this.page2.group);
+      await this.page2.group.animate(
+        [
+          {
+            x: -600 * 1.5,
+            y: 1800 * 1.5,
+            anchor: 0.5,
+            scale: 5.5
+          },
+          {
+            x: 375 * 1.5,
+            y: 668 * 1.5,
+            anchor: 0.5,
+            scale: 1
+          }
+        ],
+        {
+          duration: 1000,
+          fill: "forwards"
+        }
+      ).finished;
+
+      this.autoAnimatPage2();
+    },
+    autoAnimatPage2() {
+      this.interval = setInterval(() => {
+        this.page2.group.clear();
+        this.page2.group.append(this.page2.background);
+        this.page2.group.append(this.page1.group);
+        this.page2.group.append(this.page2.Man1[this.page2.Man1Index]);
+        this.page2.group.append(this.page2.Girl2[this.page2.Girl2Index]);
+        this.page2.group.append(this.page2.Girl3[this.page2.Girl3Index]);
+        this.page2.group.append(this.page2.Eye4[this.page2.Eye4Index]);
+        this.page2.group.append(this.page2.Girl5[this.page2.Girl5Index]);
+        this.page2.group.append(this.page2.Man6[this.page2.Man6Index]);
+        this.nextFrame("Man1Index", this.page2.Man1.length, 0, "page2");
+        this.nextFrame("Girl2Index", this.page2.Girl2.length, 0, "page2");
+        this.nextFrame(
+          "Girl3Index",
+          this.page2.Girl3.length,
+          0,
+          "page2",
+          "once"
+        );
+        this.nextFrame("Eye4Index", this.page2.Eye4.length, 0, "page2");
+        this.nextFrame("Girl5Index", this.page2.Girl5.length, 0, "page2");
+        this.nextFrame("Man6Index", this.page2.Man6.length, 0, "page2");
+      }, 100);
+    },
+    async Page2TransToPage3() {
+      clearInterval(this.interval);
+      this.transto3.group = new Group();
+      this.layer.append(this.transto3.group);
+      this.transto3.group.attr({
+        pos: [750 * 1.5, 0],
+        zIndex: 5
+      });
+      this.page3.transBackground = new Sprite(
+        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page3/%E4%B8%AD%E9%97%B4%E8%BF%87%E6%B8%A1.jpg"
+      );
+      this.transto3.group.append(
+        this.page3.transBackground.attr({
+          width: 711 * 1.5,
+          height: 1331 * 1.5
+        })
+      );
+      await this.page2.group.animate(
+        [
+          {
+            scale: 1,
+            x: 562.5,
+            y: 1002
+          },
+          {
+            scale: 1.6,
+            x: 224,
+            y: 449
+          }
+        ],
+        {
+          duration: 1000,
+          fill: "forwards"
+        }
+      ).finished;
+      this.transto3.group.attr({
+        x: 1975,
+        y: 448,
+        scale: 1.6,
+        anchor: 0.5
+      });
+      this.page2.group.animate([{ x: 224 }, { x: -960 }], {
+        duration: 1000,
+        fill: "forwards"
+      });
+      await this.transto3.group.animate([{ x: 1975 }, { x: 781 }], {
+        duration: 1000,
+        fill: "forwards"
+      }).finished;
+      await this.transto3.group.animate([{ x: 781 }, { x: 270 }], {
+        duration: 500,
+        fill: "forwards"
+      }).finished;
+      this.loadAssetsPage3();
+    },
+    async loadAssetsPage3() {
+      this.page3.background = new Sprite(
+        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page3/%E6%AD%8C%E7%8E%8B%E8%83%8C%E6%99%AF.jpg"
+      ).attr({
+        width: 750 * 1.5,
+        height: 1336 * 1.5
+      });
+      this.page3.group = new Group();
+      this.layer.append(this.page3.group);
+      this.page3.group.attr({
+        zIndex: 6,
+        anchor: 0.5,
+        // x: 1950,
+        x: 2020,
+        y: 450,
+        scale: 1.6
+      });
+      this.page3.group.append(this.page3.background);
+      this.transto3.group.animate([{ x: 270 }, { x: -950 }], {
+        // this.transto3.group.animate([{ x: 270 }, { x: 200 }], {
+        duration: 500,
+        fill: "forwards"
+      });
+      await this.page3.group.animate([{ x: 2020 }, { x: 800 }], {
+        duration: 500,
+        fill: "forwards"
+      }).finished;
+      this.page3.group.animate(
+        [
+          { scale: 1.6, x: 800, y: 450 },
+          { scale: 1, x: 375 * 1.5, y: 668 * 1.5 }
+        ],
+        {
+          duration: 500,
+          fill: "forwards"
+        }
+      );
+      for (let index = 0; index <= 11; index++) {
+        const Girl = new Sprite(`
+        https://test-1255639802.cos.ap-beijing.myqcloud.com/page3/girl/%E6%AD%8C%E7%8E%8B-%E5%8A%A8%E7%94%BB%E5%87%86%E5%A4%87_000${
+          index.toString().length === 1 ? `0${index}` : index
+        }-min.png
+        `);
+        Girl.attr({ zIndex: 6 });
+        this.page3.Girl.push(
+          Girl.attr({
+            width: 260 * 1.5,
+            height: 600 * 1.5,
+            pos: [226 * 1.5, 510 * 1.5]
+          })
+        );
+      }
+      this.page3.group.append(this.page3.Girl[0]);
+      this.autoAnimatPage3();
+    },
+    autoAnimatPage3() {
+      this.interval = setInterval(() => {
+        this.page3.group.clear();
+        this.page3.group.append(this.page3.background);
+        this.page3.group.append(this.page3.Girl[this.page3.GirlIndex]);
+
+        this.nextFrame("GirlIndex", this.page3.Girl.length, 0, "page3");
+      }, 100);
+    },
+    Page3TransToPage4() {
+      clearInterval(this.interval);
+      this.transto4.group = new Group();
+      this.transto4.Sky = new Sprite(
+        "https://test-1255639802.cos.ap-beijing.myqcloud.com/page3/%E5%A4%A9%E7%A9%BA%E8%83%8C%E6%99%AF.jpg"
+      );
+      window.group = this.transto4.group.attr({
+        zIndex: 999
+      });
+      this.transto4.Sky.attr({
+        width: 750 * 1.5,
+        height: 1462 * 1.5,
+        zIndex: 6,
+        anchor: 0.5,
+        pos: [-375 * 1.5, -1462 * 1.5]
+      });
+      this.transto4.group.attr({
+        width: 750 * 1.5,
+        height: 1462 * 1.5,
+        zIndex: 6,
+        anchor: 0.5,
+        pos: [-375 * 1.5, -1462 * 1.5]
+      });
+      this.transto4.group.append(this.transto4.Sky);
+      this.layer.append(this.transto4.group);
+      // this.page3.group.animate([{ y: 668 * 1.5 }, { y: 668 * 1.5 + 200 }], {
+      //   duration: 500,
+      //   fill: "forwards"
+      // });
+      this.transto4.group.animate(
+        [{ pos: [-375 * 1.5, -1462 * 1.5] }, { pos: [0, 0] }],
+        {
+          duration: 500,
+          fill: "forwards"
+        }
+      );
+      // this.page3.
     },
     loadAssetsPage4() {
       const background = new Sprite(
