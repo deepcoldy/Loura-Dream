0a0de3238855b4cdc11cf7e462f9101d917cdf7b
申晗 <deepcoldy@gmail.com>
2018-08-13 19:30:06 +0800

    add config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -54,6 +54,14 @@ export default {
   methods: {
     setWechatShare() {
       wx.ready(function() {
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: "wxc86d7e7d91123720", // 必填，企业号的唯一标识，此处填写企业号corpid
+          timestamp: new Date().getTime(), // 必填，生成签名的时间戳
+          nonceStr: "", // 必填，生成签名的随机串
+          signature: "", // 必填，签名，见附录1
+          jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
         wx.onMenuShareAppMessage({
           title: "娄艺潇的圆梦之旅", // 分享标题
           desc: "娄艺潇的圆梦之旅", // 分享描述
