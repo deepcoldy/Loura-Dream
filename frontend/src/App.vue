dd9c55f512d815eef879720cd6fc5b40ba7db753
DeepCold <deepcold@shenhans-iMac.lan>
2018-08-13 21:43:38 +0800

    get wechat config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,6 +19,7 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
@@ -53,37 +54,32 @@ export default {
   },
   methods: {
     setWechatShare() {
-      wx.ready(function() {
-        wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: "wxc86d7e7d91123720", // 必填，企业号的唯一标识，此处填写企业号corpid
-          timestamp: new Date().getTime(), // 必填，生成签名的时间戳
-          nonceStr: "", // 必填，生成签名的随机串
-          signature: "", // 必填，签名，见附录1
-          jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        });
-        wx.onMenuShareAppMessage({
-          title: "娄艺潇的圆梦之旅", // 分享标题
-          desc: "娄艺潇的圆梦之旅", // 分享描述
-          link: "https://loura.beats-digital.com", // 分享链接，该链接域名必须与当前企业的可信域名一致
-          imgUrl: "https://cdn.zoocer.com/share.jpg", // 分享图标
-          success: function() {
-            // 用户确认分享后执行的回调函数
-          },
-          cancel: function() {
-            // 用户取消分享后执行的回调函数
-          }
-        });
-        wx.onMenuShareTimeline({
-          title: "娄艺潇的圆梦之旅", // 分享标题
-          link: "https://loura.beats-digital.com",
-          imgUrl: "https://cdn.zoocer.com/share.jpg", // 分享图标
-          success: function() {
-            // 用户确认分享后执行的回调函数
-          },
-          cancel: function() {
-            // 用户取消分享后执行的回调函数
-          }
+      axios.get("/api/wechat/").then(resp => {
+        wx.ready(() => {
+          wx.config(resp);
+          wx.onMenuShareAppMessage({
+            title: "娄艺潇的圆梦之旅", // 分享标题
+            desc: "娄艺潇的圆梦之旅", // 分享描述
+            link: "https://loura.beats-digital.com", // 分享链接，该链接域名必须与当前企业的可信域名一致
+            imgUrl: "https://cdn.zoocer.com/share.jpg", // 分享图标
+            success: function() {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function() {
+              // 用户取消分享后执行的回调函数
+            }
+          });
+          wx.onMenuShareTimeline({
+            title: "娄艺潇的圆梦之旅", // 分享标题
+            link: "https://loura.beats-digital.com",
+            imgUrl: "https://cdn.zoocer.com/share.jpg", // 分享图标
+            success: function() {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function() {
+              // 用户取消分享后执行的回调函数
+            }
+          });
         });
       });
     },
